87c812e1c160496396877e7bbf55bdcbc454e9b1
mdrahib46 <nahid179846@bsdi-bd.org>
2026-03-06 01:29:42 +0600

    feat: intro-commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/screens/login_screen.dart';
+import 'package:task_manager_app/screens/splash_screen.dart';
+import 'package:task_manager_app/utils/themes/light_theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      themeMode: ThemeMode.system,
+      theme: lightTheme,
+      home: const LoginScreen(),
     );
   }
 }
---
+++ lib/utils/asset_path.dart
@@ -0,0 +1,7 @@
+class AssetPath {
+
+  static const String _imagePath = 'assets/images';
+
+  static const String bgSVGImage = '$_imagePath/background.svg';
+  static const String logoSVGImage = '$_imagePath/logo.svg';
+}

3bdc310eb6960d7d24808f48fd2462c09b049f42
mdrahib46 <nahid179846@bsdi-bd.org>
2026-03-06 01:30:26 +0600

    feat: implemented splash_screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:task_manager_app/screens/login_screen.dart';
+import 'package:task_manager_app/utils/asset_path.dart';
+import 'package:task_manager_app/widgets/custom_app_background.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if(mounted){
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+            (route) => false,
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomAppBackground(
+        child: Center(
+          child: SvgPicture.asset(AssetPath.logoSVGImage, height: 64),
+        ),
+      ),
+    );
+  }
+}

df21da2fed6b533b6def884e85963411554c12b6
mdrahib46 <nahid179846@bsdi-bd.org>
2026-03-07 01:43:50 +0600

    feat: implemented Pin Code Verification Screen

---
+++ lib/screens/pin_verify_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_app/widgets/auth_prompt_text_button.dart';
+import 'package:task_manager_app/widgets/custom_app_background.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class PinVerificationScreen extends StatefulWidget {
+  const PinVerificationScreen({super.key});
+
+  @override
+  State<PinVerificationScreen> createState() => _PinVerificationScreenState();
+}
+
+class _PinVerificationScreenState extends State<PinVerificationScreen> {
+  final PinInputController _pinCodeController = PinInputController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomAppBackground(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Pin Verification',
+                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                'A 6 digit verification pin has sent to your email address',
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 16),
+              _buildPinCodeInput(),
+              const SizedBox(height: 20,),
+              ElevatedButton(onPressed: (){}, child: Text('Verify')),
+              const SizedBox(height: 40,),
+              AuthPromptTextButton(promptText: 'Have account? ', actionText: 'Sign in', onTap: (){})
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPinCodeInput() {
+    return PinInput(
+      length: 6,
+      pinController: _pinCodeController,
+      // scrollPadding: EdgeInsets.zero,
+      keyboardType: TextInputType.number,
+      builder: (context, cells) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: cells.map((cell) {
+            return Container(
+              width: 45,
+              height: 50,
+              margin: const EdgeInsets.symmetric(horizontal: 8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  width: cell.isFocused ? 2 : 1,
+                  color: cell.isFocused ? Colors.green : Colors.transparent,
+                ),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                cell.character ?? '',
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  void _clearText() {
+    _pinCodeController.clear();
+  }
+}

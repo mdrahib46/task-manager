f16aba88b21878984e29a606cfde6865808e44b1
mdrahib46 <nahid179846@bsdi-bd.org>
2026-03-16 15:24:39 +0600

    feat: Implemented in-progress task screen API

--- lib/screens/inProgress_task_screen.dart
+++ lib/screens/inProgress_task_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/data/models/network_response.dart';
+import 'package:task_manager_app/data/models/task_model.dart';
+import 'package:task_manager_app/data/services/api_response.dart';
+import 'package:task_manager_app/utils/app_urls.dart';
+import 'package:task_manager_app/widgets/snackbar_message.dart';
 
 import 'package:task_manager_app/widgets/task_card_tile.dart';
-
 
 class InProgressTaskScreen extends StatefulWidget {
   static const String name = 'InProgress-Task';
@@ -12,30 +16,66 @@ class InProgressTaskScreen extends StatefulWidget {
 }
 
 class _InProgressTaskScreenState extends State<InProgressTaskScreen> {
+  bool _inProgress = false;
+  List<TaskModel> _pendingTaskList = [];
+
+  @override
+  void initState() {
+    _getPendingTask();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          child: ListView.builder(
-            padding: EdgeInsets.zero,
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return TaskCardTile(
-                title: 'What is Lorem Ipsum?',
-                subTitle:
-                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,',
-                status: 'Progress',
-
-                date: 'Date: ${DateTime.now().toString().split(' ')[0]}',
-                onTapEdit: () {},
-                onTapDelete: () {},
-              );
-            },
+          child: Visibility(
+            visible: !_inProgress,
+            replacement: Center(child: CircularProgressIndicator()),
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: _pendingTaskList.length,
+              itemBuilder: (context, index) {
+                final task = _pendingTaskList[index];
+                return TaskCardTile(
+                  title: task.title ?? '',
+                  subTitle: task.description ?? '',
+                  status: task.status ?? '',
+                  date: 'Date: ${task.createdDate ?? ''}',
+                  onTapEdit: () {},
+                  onTapDelete: () {},
+                );
+              },
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _getPendingTask() async {
+    _inProgress = true;
+    setState(() {});
+    final NetworkResponse response = await ApiCaller.getRequest(
+      url: AppUrls.getPendingTask,
+    );
+    _inProgress = false;
+    setState(() {});
+    if (response.isSuccess) {
+      final TaskListModel taskListModel = TaskListModel.fromJson(
+        response.responseData,
+      );
+      _pendingTaskList = taskListModel.data ?? [];
+    } else {
+      if (mounted) {
+        showSnackBarMessage(
+          context: context,
+          message: response.errorMessage,
+          isError: false,
+        );
+      }
+    }
   }
 }
